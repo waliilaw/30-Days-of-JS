0a3f3c85197d8a48b5e8d12f98a0d9c9e3a0e558
waliilaw <humaidwali20@gmail.com>
2024-07-14 14:16:17 +0530

    DAY01.js

---
+++ DAY01.js
@@ -0,0 +1,72 @@
+// Variables and Data Types //
+
+// Activity 1 : variable declaration
+
+var VariableOne = 15
+console.log(VariableOne);
+
+let StringOne = "Chosen"
+console.log(StringOne);
+
+// Activity 2 : Constant declaration
+
+const BooleanOne = true 
+console.log(BooleanOne);
+
+// Activity 3 : Data Types
+
+let Var1 = 69
+let Var2 = "Hi"
+let Var3 = true 
+let Var4 = null
+let Var5 =[1, 2,3, 4]
+
+console.log(typeof Var1);
+console.log(typeof Var2);
+console.log(typeof Var3);
+console.log(typeof Var4);
+console.log(typeof Var5);
+
+// Activity 4 : Reassingning Variables
+
+let Newvalue = "wali"
+console.log(Newvalue);
+
+Newvalue = "hello"
+console.log(Newvalue);
+
+// Activity 5 : Understanding const 
+
+const Errorone = "abdul"
+console.log(Errorone);
+
+// Feature Request 
+
+// 1
+
+let NumberOne = 190
+let StringTwo = "wali"
+let BooleanTwo = false
+let ObjectOne={
+    name : "hitesh",
+    place : "gaya",
+}
+let ArrayOne = [1,2,3,4,5]
+
+console.log(`${NumberOne} and Type is ${typeof NumberOne}`);
+console.log(`${StringTwo} and Type is ${typeof StringTwo}`);
+console.log(`${BooleanTwo} and Type is ${typeof BooleanTwo}`);
+console.log(`${ObjectOne} and Type is ${typeof ObjectOne}`);
+console.log(`${ArrayOne} and Type is ${typeof ArrayOne}`);
+
+// 2
+
+let ReassignValue = "First"
+ReassignValue = "Second"
+
+console.log(`Changed value is ${ReassignValue}`);
+
+// const ErrorTwo = "One"
+// ErrorTwo = "Two"
+
+// console.log(`Changed value is ${ErrorTwo}`);
