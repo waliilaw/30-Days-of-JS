4cc44a72a4a829de7e15893f49a054714eb255c7
Wali <humaidwali20@gmail.com>
2024-08-06 23:16:07 +0530

    Create DAY02.js

---
+++ DAY02.js
@@ -0,0 +1,32 @@
+// OPERATORS //
+
+// Activity 1 
+
+console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3/3);
+console.log(3%3);
+
+// Activity 2 
+
+let TwoNum = 3;
+TwoNum += 3;
+console.log(TwoNum);
+
+let SubNum = 3;
+SubNum += 3;
+console.log(SubNum);
+
+// Activity 3
+
+console.log(3>4);
+console.log(3>=3);
+console.log(3==="3");
+
+// Activity 4
+
+console.log(10>9 && 3>4);
+console.log(10>9 || 3>4);
+
+
